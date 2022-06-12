b246e9eed2398049fa410ba2fc7d1b719f64ee62
Andrew Terry <andrew@andrewterry.com>
2022-06-12 15:57:17 +0000

    build login page, and navigation links to login and home

---
+++ test/system/user_logins_test.rb
@@ -0,0 +1,22 @@
+require "application_system_test_case"
+
+class UserLoginsTest < ApplicationSystemTestCase
+  # It would have been nice to follow the TDD/TFD principle started in the 
+  # AlphaBlog section, but it hasn't been mentioned in this section...
+  # I'll leave this for another day, when I know more.
+  #
+  # driven_by :selenium, using: :headless_chrome
+
+  # test "visiting the index" do
+  #   visit user_logins_url
+  #
+  #   assert_selector "h1", text: "UserLogin"
+  # end
+  
+  # test "visit the login page" do
+  #   visit login_url
+    
+  #   assert_selector "p", text: "The login page"
+  # end
+  
+end
