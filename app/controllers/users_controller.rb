11ddc629dea6ddfc5dfb14be6220f36d6e1d0fbf
Andrew Terry <andrew@andrewterry.com>
2022-07-15 08:05:28 +0000

    fix duplicate user sign-up flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome, #{@user.username}, you have successfully signed up!"
       redirect_to login_path
     else
+      flash[:error] = @user.errors.full_messages[0]
       render 'new'
     end
   end
